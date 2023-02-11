34d91abb9c77fbc72d9ea7adc4263b5c6d930b62
Maks <maks-lug333@yandex.ru>
2023-02-11 21:50:38 +0900

    Большее число

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1 числа");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int a = int.Parse(Console.ReadLine());
+int max = x;
+if (max < y);
+{
+     max = y;
+}
+if (max < a);
+{
+    max = a;
+}
+Console.WriteLine("Большее число:  " +max);
